1623bd5804103f6ce22651ef3f57516724d943de
Yoshihito Ogawa <yoshihito.ogawa.kc@renesas.com>
2025-10-01 13:55:24 +0900

    Docs: announcements as markdown
    - from this commit applied,
      all announcements are prepare inside docs/docs/_annoucements/announcements.md
    - announcements.md file will be loaded by mkdocs_event_hook.py,
      and will replace predefined `extra: announcements:` at mkdocs.yml.
    
    Signed-off-by: Djuned Fernando Djusdek <djunedfernando.djusdek.wz@renesas.com>

---
+++ docs/docs/_announcements/announcements.md
@@ -0,0 +1,1 @@
+**Announcement!** RZ/G HMI SDK v2.3.1.0 is available.
--- docs/mkdocs_event_hook.py
+++ docs/mkdocs_event_hook.py
@@ -12,6 +12,10 @@ def on_shutdown(**kwargs):
     base = pwd + '/docs/history/_components/changelogs'
     remove_symlink(base)
 
+def on_config(config, **kwargs):
+    target = pwd + '/docs/_announcements/announcements.md'
+    update_announcements(config, target)
+
 # ------------------------------------------------------------------------------
 
 def create_symlink(base):
@@ -60,3 +64,33 @@ def remove_symlink(base):
 
     dst = base + '/latest'
     os.remove(dst)
+
+# ------------------------------------------------------------------------------
+
+def update_announcements(config, target):
+    import markdown
+    from bs4 import BeautifulSoup
+
+    with open(target, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+
+    html = markdown.markdown(text, extensions=[
+        'tables', 'markdown_grid_tables', 'admonition',
+        'pymdownx.highlight', 'pymdownx.inlinehilite', 'pymdownx.snippets',
+        'pymdownx.superfences', 'attr_list'])
+    soup = BeautifulSoup(html, features="lxml")
+
+    announcements_list = []
+
+    if len(soup) > 0:
+        for el in soup.body:
+            el = str(el).replace('\xa0', '&nbsp;')
+            if el == "\n":
+                continue
+            elif "<p>" in el[:3]:
+                announcements_list.append(el[3:-4])
+            else:
+                announcements_list.append(el)
+
+    extra = config['extra']
+    extra['announcements'] = announcements_list
